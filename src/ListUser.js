d2ab2321502b127a4845eba438d96e4a7571c648
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-07-17 15:21:34 +0700

    update

--- src/ListUser.js
+++ src/ListUser.js
@@ -1,26 +1,78 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import axios from "axios";
+import Swal from "sweetalert2";
 
 
 const User = () => {
-
-  const [email, setEmail] = useState("");
   const [name, setName] = useState("");
-  const navigate = useNavigate();
+  const [tgl_lahir, setTanggalLahir] = useState("");
+  const [tahunlhr, setTahun] = useState();
+  const [bulanlhr, setBulan] = useState();
+  const [alamat, setAlamat] = useState("");
+  const [email, setEmail] = useState("");
+  // const navigate = useNavigate();
+
+  function cekUmur(tgllahir) {
+    const hariIni = new Date();
+    const tahunIni = hariIni.getFullYear();
+    const bulanIni = hariIni.getMonth();
+    const tanggal = hariIni.getDate();
+
+    const tahunLahir = tgllahir.getFullYear();
+    const bulanLahir = tgllahir.getMonth();
+    const tanggalLahirTanggal = tgllahir.getDate();
+
+    let umurTahun = tahunIni - tahunLahir;
+    let umurBulan = bulanIni - bulanLahir;
+
+    if (bulanIni < bulanLahir || (bulanIni === bulanLahir && tanggal < tanggalLahirTanggal)) {
+      umurTahun--;
+      umurBulan += 12;
+    }
+
+    return { tahun: umurTahun, bulan: umurBulan };
+  }
+
+  const handleUmur = async (usia) => {
+    setTanggalLahir(usia);
+    const [thn, bln, tgl] = usia.split('-').map(Number);
+    const tanggalLahirnya = new Date(thn, bln - 1, tgl);
+    const usianya = cekUmur(tanggalLahirnya);
+    setTahun(usianya.tahun)  
+    setBulan(usianya.bulan);
+  }
 
   const check = async () => {
     const body = {
       "name": name,
+      "birth_date": tgl_lahir,
+      "age_years": tahunlhr,
+      "age_months": bulanlhr,
+      "address": alamat,
       "email": email
     }
-    axios.post('https://api.example.com/new-item', JSON.stringify(body))
+
+    const header = {
+      'Content-Type': 'application/json',
+    }
+    axios.post('https://nci.rifhandi.com/api/user', body , { headers : header })
       .then(response => {
-        console.log(response.data);
-        navigate('/')
+       console.log(response.data);
+        // Swal.fire({
+        //   icon: "success",
+        //   title: "Success",
+        //   text: "Data Berhasil Disimpan!",
+        // });
+        // navigate('/konfirmasi')
       })
       .catch(error => {
-        console.error('Error creating item:', error);
+        // Swal.fire({
+        //   icon: "error",
+        //   title: "Error",
+        //   text: error,
+        // });
+         console.log(error);
       });
 
   }
@@ -50,25 +102,32 @@ const User = () => {
         <div className="form-group row">
           <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Nama</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+            <input type="text" className="form-control" id="name" onChange={(e) => { setName(e.target.value) }} required />
           </div>
         </div>
         <div className="form-group row">
           <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Tanggal Lahir</label>
           <div className="col-sm-10">
-            <input type="date" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+            <input type="date" className="form-control" id="birth_date" onChange={(e) => handleUmur(e.target.value)} required />
           </div>
         </div>
         <div className="form-group row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Umur</label>
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Umur(Tahun)</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+            <input type="number" className="form-control" id="age_years" onChange={(e) => {setTahun(e.target.value) }}  value={tahunlhr || 0} required />
           </div>
         </div>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Umur(Bulan)</label>
+          <div className="col-sm-10">
+            <input type="number" className="form-control" id="age_months" onChange={(e) => {setBulan(e.target.value) }} value={bulanlhr || 0} required />
+          </div>
+        </div>
+        {/* defaultValue={0} ini pada value form html */}
         <div className="form-group row">
           <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Alamat</label>
           <div className="col-sm-10">
-            <input type="address" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+            <input type="address" className="form-control" id="address " onChange={(e) => { setAlamat(e.target.value) }} required />
           </div>
         </div>
         <div className="form-group row">
@@ -77,7 +136,7 @@ const User = () => {
             <input type="email" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
           </div>
         </div>
-         <div className="form-group row">
+        <div className="form-group row">
           <label htmlFor="staticEmail" className="col-sm-2 col-form-label">-</label>
           <div className="col-sm-10">
             <button type="button" className="btn btn-primary" onClick={check}>Submit</button> &nbsp; &nbsp;
