c9a735288028c4c83a6cce262148121748761c4f
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-07-17 14:10:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ListUser from './ListUser';
 import DetailUser from './DetailUser';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route exact path='/' component={ListUser} />
-        <Route path='/detail/:id_user' component={DetailUser} />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<ListUser />} />
+        <Route path='/user/:id_user' element={<DetailUser />} />
+      </Routes>
+    </Router>
   )
 
 }
--- src/ListUser.js
+++ src/ListUser.js
@@ -1,49 +1,95 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 
+const User = () => {
 
-const Add = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    // const em = document.getElementById("emails").value;
-    // console.log(em);
-
-    const check = async () => {
-        console.log(email)
-        console.log(password)
-
-        fetch('url', {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({})
-        })
-            .then(response => response.json())
-            .then(data => {
-                navigate('/')
-            })
-
+  const check = async () => {
+    const body = {
+      "name": name,
+      "email": email
     }
+    axios.post('https://api.example.com/new-item', JSON.stringify(body))
+      .then(response => {
+        console.log(response.data);
+        navigate('/')
+      })
+      .catch(error => {
+        console.error('Error creating item:', error);
+      });
 
-    return (
-        <div>
-            {/* <input type="text" id="emails" onChange={(e)=>{e.target.value}}></input>
-           <br />
-           <button type="button" onClick={check}>Submit</button> */}
+  }
 
-            <input type="email" id="email" onChange={(e) => { setEmail(e.target.value) }} required></input>
-            <input type="password" id="password" onChange={(e) => { setPassword(e.target.value) }} required></input>
-            <button type="button" onClick={check}>Submit</button>
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#t">
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg" width="30" height="30" className="d-inline-block align-top" alt="Logo Anda" />
+          &nbsp; &nbsp; NCI APPS
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#tnavbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#t">Home <span className="sr-only">(current)</span></a>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="#t">Tentang Kami</a>
+            </li> */}
+          </ul>
         </div>
-    )
+      </nav>
+      <form>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Nama</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Tanggal Lahir</label>
+          <div className="col-sm-10">
+            <input type="date" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Umur</label>
+          <div className="col-sm-10">
+            <input type="number" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Alamat</label>
+          <div className="col-sm-10">
+            <input type="address" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="email" onChange={(e) => { setEmail(e.target.value) }} required />
+          </div>
+        </div>
+         <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">-</label>
+          <div className="col-sm-10">
+            <button type="button" className="btn btn-primary" onClick={check}>Submit</button> &nbsp; &nbsp;
+            <button type="reset" className="btn btn-success">Reset</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 
 
 }
 
-export default Add
+export default User
 
